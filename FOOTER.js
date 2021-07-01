7a2b5583c792f3b5afe52c5289bcecf88dd9d023
MichaelTaveras <78162666+MichaelTaveras@users.noreply.github.com>
2021-06-30 21:28:03 -0400

    Add files via upload

---
+++ FOOTER.js
@@ -0,0 +1,5 @@
+document.write("<CENTER><table><TR height=\"50\"></tr><tr><td width = \"100%\">")
+document.write("  	<p align=\"center\"><font size=\"2\">");
+document.write("		<strong>Conference Paper Management System</strong>");
+document.write("        <BR/><strong>Copyright 2021 - G15 Enterprise </strong><BR/>");
+document.write("		<strong>All rights reserved.</strong><BR/>");
