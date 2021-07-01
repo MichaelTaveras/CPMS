7bfcf9a411c836138c04ec49c446c174fe1cf479
MichaelTaveras <78162666+MichaelTaveras@users.noreply.github.com>
2021-06-30 22:38:54 -0400

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,3 +1,4 @@
 document.write('<div class="topnav"><a href="index.html"><img src="images/CPMSLogo.png" style="height: 125px; padding: 10px;"></a>');
 document.write('<a href="#" class="link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/></svg></a>');
-document.write('<a href="maintenance.html" class="link">Maintenance</a><a href="#" class="link">Upload</a><a href="#"class="link">View</a></div>')
+document.write('<a href="#" class="link">Upload</a><a href="#"class="link">View</a>');
+document.write('<div class="dropdown"><button class="dropbtn">Admin</button><div class="dropdown-content"><a href="#">Link 1</a><a href="#">Link 2</a><a href="#">Link 3</a></div></div>');
