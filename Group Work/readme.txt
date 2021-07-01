4d8c9787702cbe2dca3b5dc002630b4944b8e5ed
MichaelTaveras <78162666+MichaelTaveras@users.noreply.github.com>
2021-07-01 15:47:01 -0400

    Add files via upload

---
+++ Group Work/readme.txt
@@ -0,0 +1,3 @@
+Group 15 Project- Michael Taveras, Justina Le, Camila Vizcaya, Reynaldo Sanchez
+
+Readme file where we add info as needed.
---
+++ Group Work/scripts/nav.js
@@ -0,0 +1,5 @@
+document.write('<link rel="stylesheet" href="styles/stylesNAV.css">');
+document.write('<div class="topnav"><a href="index.html"><img src="CPMSLogo.png" style="height: 125px; padding: 10px;"></a>');
+document.write('<a href="RegistrationForm.html" class="link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/></svg></a>');
+document.write('<a href="paperSubmitForm.html" class="link">Upload</a><a href="#"class="link">View</a>');
+document.write('<div class="dropdown"><button class="dropbtn">Admin</button><div class="dropdown-content"><a href="PaperReview.html">Paper Reviews</a><a href="Management.html">Management</a><a href="#">Link 3</a></div></div>');
