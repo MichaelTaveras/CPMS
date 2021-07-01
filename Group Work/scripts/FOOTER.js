a62a9dc4134b9fac3e692b9874606c6fe093e5dc
Tina Le <justinale725@gmail.com>
2021-07-01 16:19:53 -0400

    added PaperReview and image and updated footer

--- Group Work/scripts/FOOTER.js
+++ Group Work/scripts/FOOTER.js
@@ -1,4 +1,4 @@
-document.write("<CENTER><table><TR height=\"50\"></tr><tr><td width = \"100%\">")
+document.write("<CENTER><table class=\"footer\"><TR height=\"50\"></tr><tr><td class=\"footer\" width = \"100%\">")
 document.write("  	<p align=\"center\"><font size=\"2\">");
 document.write("		<strong>Conference Paper Management System</strong>");
 document.write("        <BR/><strong>Copyright 2021 - G15 Enterprise </strong><BR/>");
