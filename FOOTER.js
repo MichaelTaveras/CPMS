33d62ee4738b61338c2abd6534699ce77ba999af
MichaelTaveras <78162666+MichaelTaveras@users.noreply.github.com>
2021-07-02 01:43:03 -0400

    Delete FOOTER.js

--- FOOTER.js
+++
@@ -1,5 +0,0 @@
-document.write("<CENTER><table class=\"footer\"><TR height=\"50\"></tr><tr><td class=\"footer\" width = \"100%\">")
-document.write("  	<p align=\"center\"><font size=\"2\">");
-document.write("		<strong>Conference Paper Management System</strong>");
-document.write("        <BR/><strong>Copyright 2021 - G15 Enterprise </strong><BR/>");
-document.write("		<strong>All rights reserved.</strong><BR/>");
