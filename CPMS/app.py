e1e55c6c7c4b813c17dbfba7567a5c4991d93a35
Rey Sanchez <reyjr98@gmail.com>
2021-07-09 15:20:14 -0400

    DB setup, base.html began, account settings began (#3)

--- CPMS/app.py
+++ CPMS/app.py
@@ -1,34 +1,16 @@
 from os import remove
 from flask import Flask, render_template, request, send_from_directory, url_for, redirect, flash, session, Blueprint
 import sqlalchemy
+from sqlalchemy.sql.functions import user
 from .models import Author, Reviewer
 from werkzeug.security import generate_password_hash, check_password_hash # hides password
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 
+# user=current_user  -> links current user to each template
 
-
-# app = Flask(__name__ )
-# app.config['SECRET_KEY'] = 'iTried'
 
 app = Blueprint('app', __name__)
-
-@app.route('/')
-@login_required #cant access page unless user logged in
-def index():
-    return render_template('PaperPage.html')
-
-@app.route('/AccountSettings')
-def AccountSettings():
-    return render_template('AccountSettings.html')
-
-@app.route('/authorViewForm')
-def authorViewForm():
-    return render_template('authorViewForm.html')
-
-@app.route('/Forgot')
-def Forgot():
-    return render_template('Forgot.html')
 
 @app.route('/LoginForm', methods=['GET','POST'])
 def LoginForm():
@@ -37,42 +19,37 @@ def LoginForm():
         password = request.form.get('Password')
         remember  = request.form.get('remember')
 
+        # looks for user in DB
         author = Author.query.filter_by(EmailAddress='email').first()
+        reviewer = Reviewer.query.filter_by(EmailAddress='email').first()
 
+        # if user found checks password
+        # if wrong password throw error
+        # if user not found throw error
         if author:
             if check_password_hash(author.Password, password):
                  flash('You have been successfully logged in.', category='succes')
                  login_user(author, remember=remember)
-                 return redirect(url_for('index'))
+                 return redirect(url_for('app.index'))
+            else:
+                 flash('Wrong password. Please try again.', category='error')
+        elif reviewer:
+            if check_password_hash(reviewer.Password, password):
+                 flash('You have been successfully logged in.', category='succes')
+                 login_user(reviewer, remember=remember)
+                 return redirect(url_for('app.index'))
             else:
                  flash('Wrong password. Please try again.', category='error')
         else:
             flash('Email does not exist.  Please try again.', category='error')
 
     return render_template('LoginForm.html', user=current_user)
-        
 
-@app.route('/logout')
-@login_required #cant access page unless user logged in
-def LogOut():
-    logout_user()
-    return redirect(url_for('LoginForm'))
 
-@app.route('/Management')
-def Management():
-    return render_template('Management.html')
+@app.route('/Forgot')
+def Forgot():
+    return render_template('Forgot.html')
 
-@app.route('/PaperPage')
-def PaperPage():
-    return render_template('PaperPage.html')
-
-@app.route('/PaperReview')
-def PaperReview():
-    return render_template('PaperReview.html')
-
-@app.route('/paperSubmitForm')
-def paperSubmitForm():
-    return render_template('paperSubmitForm.html')
 
 @app.route('/RegistrationForm',methods=['GET','POST'])
 def RegistrationForm():
@@ -139,27 +116,88 @@ def RegistrationForm():
             else:
                 print("Error on user type")
             # to database
-            # db.session.add(newUser)
-            # db.session.commit()
+            db.session.add(newUser)
+            db.session.commit()
             login_user(newUser, remember=True)
             flash('Account created!', category='success')
-            return redirect(url_for('index'))
+
+            return redirect(url_for('app.index'))
             
 
     return render_template('RegistrationForm.html', user=current_user)
 
+
+# //////////////////////////////////////////////////////////////////////
+
+
+# TODO: THESE TEMPLATES NEED TO BE IMPLEMENTED WHICH USER INFO
+
+
+@app.route('/')
+@login_required #cant access page unless user logged in
+def index():
+    # have correct things chow depending on user
+    return render_template('PaperPage.html', user=current_user)
+
+
+@app.route('/AccountSettings',methods=['GET','POST'])
+@login_required
+def AccountSettings():
+
+
+
+    return render_template('AccountSettings.html', user=current_user)
+
+
+@app.route('/authorViewForm')
+@login_required
+def authorViewForm():
+    return render_template('authorViewForm.html',user=current_user)
+
+
+@app.route('/logout')
+@login_required #cant access page unless user logged in
+def LogOut():
+    logout_user()
+    return redirect(url_for('LoginForm'))
+
+@app.route('/Management')
+@login_required
+def Management():
+    return render_template('Management.html',user=current_user)
+
+@app.route('/PaperPage')
+@login_required
+def PaperPage():
+    return render_template('PaperPage.html',user=current_user)
+
+@app.route('/PaperReview')
+@login_required
+def PaperReview():
+    return render_template('PaperReview.html',user=current_user)
+
+@app.route('/paperSubmitForm')
+@login_required
+def paperSubmitForm():
+    return render_template('paperSubmitForm.html',user=current_user)
+
+
 @app.route('/reviewerViewForm')
+@login_required
 def reviewerViewForm():
-    return render_template('reviewerViewForm.html')
+    return render_template('reviewerViewForm.html',user=current_user)
+
 
 @app.route('/reviewSubmitForm')
+@login_required
 def reviewSubmitForm():
-    return render_template('reviewSubmitForm.html')
+    return render_template('reviewSubmitForm.html',user=current_user)
+
 
 @app.route('/viewForm')
+@login_required
 def viewForm():
-    return render_template('viewForm.html')
+    return render_template('viewForm.html',user=current_user)
 
 
 
-    #app.run(host="localhost", port=8010, debug=True)
