2fce8405045a829cdd7bdfa948a09f22559e0b88
reynaldos <reyjr98@gmail.com>
2021-07-15 10:42:17 -0400

    edits

--- CPMS/static/scripts/nav.js
+++ CPMS/static/scripts/nav.js
@@ -3,5 +3,6 @@ document.write('<div class="topnav"><a href="/"><img src="static/images/CPMSLogo
 document.write('<a href="AccountSettings" class="link"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/></svg></a>');
 document.write('<a href="logout" class="link">Logout</a>');
 document.write('<a href="paperSubmitForm" class="link">Upload</a>');
+document.write('<a href="ReviewForm" class="link">Review</a>');
 document.write('<a href="viewForm"class="link">View Submissions</a>');
 document.write('<div class="dropdown"><button class="dropbtn">Admin</button><div class="dropdown-content"><a href="PaperReview">Paper Reviews</a><a href="Management">Management</a></div></div>');
